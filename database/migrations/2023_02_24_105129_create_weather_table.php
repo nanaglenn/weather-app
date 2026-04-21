37d39d31062cbb119136ef6dd3c4338d44a9aa98
nanaglenn <ansahglenn@gmail.com>
2026-04-21 01:55:37 +0000

    Update 2023_02_24_105129_create_weather_table.php

--- database/migrations/2023_02_24_105129_create_weather_table.php
+++ database/migrations/2023_02_24_105129_create_weather_table.php
@@ -12,23 +12,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('weather', function (Blueprint $table) {
-            $table->id();
-            $table->decimal('lon', 7,4);
-            $table->decimal('lat', 7,4);
-            $table->bigInteger('city_id');
-            $table->string('city', 30);
-            $table->string('country', 3);
-            $table->string('icon', 5);
-            $table->string('main', 30);
-            $table->string('description', 30);
-            $table->decimal('temp', 3,1);
-            $table->decimal('temp_min', 3,1);
-            $table->decimal('temp_max', 3,1);
-            $table->integer('pressure');
-            $table->integer('humidity');
-            $table->decimal('wind_speed', 5,2);
-            $table->timestamps();
-        });
+    $table->id();
+    $table->decimal('lon', 10, 6);
+    $table->decimal('lat', 10, 6);
+    $table->unsignedBigInteger('city_id');
+    $table->string('city');
+    $table->string('country', 10);
+    $table->string('icon', 20);
+    $table->string('main', 50);
+    $table->string('description', 100);
+    $table->decimal('temp', 5, 2);
+    $table->decimal('temp_min', 5, 2);
+    $table->decimal('temp_max', 5, 2);
+    $table->integer('pressure');
+    $table->integer('humidity');
+    $table->decimal('wind_speed', 5, 2);
+    $table->timestamp('created_at');
+});
+
     }
 
     /**
